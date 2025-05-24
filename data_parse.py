6833990b605ea744aaeadc74758de590cb93e65c
Adrian <mrmaina@protonmail.com>
2025-05-24 15:53:17 +0300

    Refactored passing functions between files

--- data_parse.py
+++ data_parse.py
@@ -1,20 +1,3 @@
 import asyncio
 
-from twitter_scraper import get_links, twitter_scrape
-from pumpportal import subscribe
-
-async def get_data():
-    try:
-        scraped_data = await twitter_scrape(get_links_func=get_links,subscribe_func=subscribe)
-        if scraped_data == None:
-            return
-        tweets, mint = scraped_data
-        print(tweets)
-    except Exception as e:
-        print(f"Error fetching tweet data: {e}")
-
-try:
-    while True:
-        asyncio.run(get_data())
-except KeyboardInterrupt:
-    print("Keyboard interrupt detected. Exiting")
+from get_links import get_links
